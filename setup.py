83ab363a79b938f69992e4c8925f91a2cbb40928
Kelly Lockhart <2926089+kelockhart@users.noreply.github.com>
2020-04-17 17:08:13 -0400

    Updated for PyPI Python3 compatibility

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def get_git_version(default="v0.0.1"):
         p.stderr.close()
         line = p.stdout.readlines()[0]
         line = line.strip()
-        return line
+        return line.decode()
     except:
         return default
 
