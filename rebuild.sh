0c9352b5db5e48f2a6b1162440df4dc7efe84d28
Taylor Jacovich <tjacovich@cfa.harvard.edu>
2023-11-22 16:00:09 -0500

    Updates to work with new version of sed. (#78)

--- rebuild.sh
+++ rebuild.sh
@@ -2,9 +2,4 @@
 set -x
 set -e
 
-if [ "`docker images adsmsg | grep adsmsg`" = "" ]; then
-    docker build -t adsmsg .
-    #docker run -it --name adsmsg adsmsg
-fi
-
-docker run --rm -v `pwd`:/app --name adsmsg adsmsg make
+docker-compose up --force-recreate
